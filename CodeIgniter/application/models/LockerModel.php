c9bb469c8c503b9a1cee5db518d6c3b2aaebc384
Zhang Xi <39937546+xzhangi@users.noreply.github.com>
2018-06-13 09:56:54 +0800

    Reconfigured project directory
    -Integrated codeigniter's framework
    -Bootstrap pages are in views folder
    -Requires view controller and models

---
+++ CodeIgniter/application/models/LockerModel.php
@@ -0,0 +1,53 @@
+<?php
+	if (!defined('BASEPATH'))
+		exit ('No direct script access allowed!');
+	
+	class LockerModel extends CI_Model
+	{
+		function __construct() 
+		{
+			// Call the Model Constructor
+			parent::__construct();
+		}
+		
+		//Get all lockers (whether available or not)
+		function get_locker_list_all()
+		{
+			$this->db->select('ID', 'Name');
+			$this->db->from('tbl_locker');
+			$query = $this->db->get();
+			$result = $query->result();
+			$list = Array();
+			
+			for ($i = 0; $i < count($result); $i++)
+			{
+				$list[$i] = (object)NULL;
+				$list[$i]->LockerID->$result[$i]->ID;
+				$list[$i]->Name->$result[$i]->Name;
+			}
+			
+			return $list;
+		}
+		
+		//Get only lockers that are available
+		function get_locker_list_available()
+		{
+			$this->db->select('ID', 'Name');
+			$this->db->from('tbl_locker');
+			$this->db->where('Is_Available', 1);
+			$this->db->where('Is_Active', 1);
+			$query = $this->db->get();
+			$result = $query->result();
+			$list = Array();
+			
+			for ($i = 0; $i < count($result); $i++)
+			{
+				$list[$i] = (object)NULL;
+				$list[$i]->LockerID->$result[$i]->ID;
+				$list[$i]->Name->$result[$i]->Name;
+			}
+			
+			return $list;
+		}
+	}
+?>
--- CodeIgniter/application/views/Home.php
+++ CodeIgniter/application/views/Home.php
@@ -11,18 +11,18 @@
     <title>Locka</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="application/assets/Default-BS/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom fonts for this template -->
-    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="application/assets/Default-BS/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
 
     <!-- Plugin CSS -->
-    <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet" type="text/css">
+    <link href="application/assets/Default-BS/vendor/magnific-popup/magnific-popup.css" rel="stylesheet" type="text/css">
 
     <!-- Custom styles for this template -->
-    <link href="css/freelancer.min.css" rel="stylesheet">
+    <link href="application/assets/Default-BS/css/freelancer.min.css" rel="stylesheet">
 
   </head>
 
@@ -100,7 +100,7 @@
           <br>
           <div id="success"></div>
           <div class="form-group" style="text-align: right;">
-            <a href="Dashboard.html" class="btn btn-primary btn-xl" role="button">Login</a>
+            <a href="Dashboard.php" class="btn btn-primary btn-xl" role="button">Login</a>
             <!--<button type="submit" class="btn btn-primary btn-xl" id="sendMessageButton">Login</button>-->
           </div>
         </form>
@@ -446,19 +446,19 @@
     </div>
 
     <!-- Bootstrap core JavaScript -->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="application/assets/Default-BS/vendor/jquery/jquery.min.js"></script>
+    <script src="application/assets/Default-BS/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
     <!-- Plugin JavaScript -->
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-    <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+    <script src="application/assets/Default-BS/vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="application/assets/Default-BS/vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
 
     <!-- Contact Form JavaScript -->
-    <script src="js/jqBootstrapValidation.js"></script>
-    <script src="js/contact_me.js"></script>
+    <script src="application/assets/Default-BS/js/jqBootstrapValidation.js"></script>
+    <script src="application/assets/Default-BS/js/contact_me.js"></script>
 
     <!-- Custom scripts for this template -->
-    <script src="js/freelancer.min.js"></script>
+    <script src="application/assets/Default-BS/js/freelancer.min.js"></script>
 
   </body>
 
